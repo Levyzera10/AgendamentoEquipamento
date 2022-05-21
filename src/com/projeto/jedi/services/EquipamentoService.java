56a7dd0b92645ab32f20376a2acdcc7594953d98
levyzera <levy.edu.11@gmail.com>
2022-05-20 22:56:50 -0300

    métodos da classe equipamentoService

--- src/com/projeto/jedi/services/EquipamentoService.java
+++ src/com/projeto/jedi/services/EquipamentoService.java
@@ -1,4 +1,17 @@
 package com.projeto.jedi.services;
 
 public class EquipamentoService {
+
+    public void listarEquipamento(){
+
+    }
+    public void salvarEquipamento(){
+
+    }
+    public void alterarEquipamento(){
+
+    }
+    public void deletarEquipamento(){
+
+    }
 }
