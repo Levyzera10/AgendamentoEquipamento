f03e8dc3fd1fb0085338da58afc8532d99afb8aa
Arthur Costa <arthurcostafla0203@gmail.com>
2022-05-26 20:42:00 -0300

    criando métodos do EquipamentoRepository

--- src/com/projeto/jedi/repositories/EquipamentoRepository.java
+++ src/com/projeto/jedi/repositories/EquipamentoRepository.java
@@ -1,4 +1,17 @@
 package com.projeto.jedi.repositories;
 
 public class EquipamentoRepository {
+
+    public void getAll(){
+
+    }
+    public void save(){
+
+    }
+    public void update(){
+
+    }
+    public void delete(){
+
+    }
 }
