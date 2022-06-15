abede6fa9cfaf3d6bc1d266d3000e8038beeaeaa
levyzera <levy.edu.11@gmail.com>
2022-06-14 22:13:26 -0300

    atributos dos equipamentos

--- src/com/projeto/jedi/model/Equipamento.java
+++ src/com/projeto/jedi/model/Equipamento.java
@@ -3,6 +3,28 @@ package com.projeto.jedi.model;
 public class Equipamento {
 
     private String nome;
+    private String codigo;
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    private String fabricante;
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
 
     public String getNome() {
         return nome;
